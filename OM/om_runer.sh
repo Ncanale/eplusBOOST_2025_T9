797f3138e279a54aff6e28b8e8bbe17dfa63f607
Ncanale <ncanale@fe.infn.it>
2025-06-15 01:56:08 +0200

    ..

--- OM/om_runer.sh
+++ OM/om_runer.sh
@@ -6,7 +6,7 @@ mkdir -p $JUPYTER_CONFIG_DIR
 targetdir="/eos/project/c/crystal-ferrara/www/"
 currRunJson="single_run_number.json"
 sourcefile="check_signal_epBOOST"
-sourcefile2="check_signal_epBOOST"
+sourcefile2="commpare_epBOOST"
 
 
 sleepsec=10git
@@ -22,7 +22,7 @@ while true; do
         echo $run_number
 
         # Update json
-        sed -i "s/\"run_number\": *[0-9]\+/\"run_number\": $run_number/" $currRunJson
+        sed -i "s/\"run_number\": *[0-9]\+/\"run_number\": $run_number/" $currRunJsongi
 
         # Execute jupyter
         currFile="run${run_number}.html"
@@ -31,7 +31,7 @@ while true; do
         jupyter nbconvert --execute --to html ${sourcefile2}.ipynb
         cp ${sourcefile}.html $targetdir/$currFile
         cp ${sourcefile}.html $targetdir/current.html
-        cp ${sourcefile2}.html $targetdir/Compare.html
+        cp ${sourcefile2}.html $targetdir/compare.html
 
         sleep ${sleepsec}s
 
