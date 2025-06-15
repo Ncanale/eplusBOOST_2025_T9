ab8364826bb67b925db1c7e58f910a3ae3cdab96
Ncanale <ncanale@fe.infn.it>
2025-06-15 02:01:22 +0200

    ..

--- OM/om_runner.sh
+++ OM/om_runner.sh
@@ -2,37 +2,35 @@
 export JUPYTER_CONFIG_DIR=/tmp/my_jupyter_config
 mkdir -p $JUPYTER_CONFIG_DIR
 
-
 targetdir="/eos/project/c/crystal-ferrara/www/"
 currRunJson="single_run_number.json"
 sourcefile="check_signal_epBOOST"
 sourcefile2="compare_epBOOST"
 
-
 sleepsec=180
 
 while true; do
 
-        # Detect current Run
-        filepath="$(ls -tr /eos/project/i/insulab-como/testBeam/TB_2025_06_T9_epBOOST/ASCII_MICHELA/ascii_daq_sshfs/*.dat  | tail -1)
-        run_number="${filepath##*/}"
-        run_number="${run_number#run}"
-        run_number="${run_number%%_*}"
+    # Detect current Run
+    filepath="$(ls -tr /eos/project/i/insulab-como/testBeam/TB_2025_06_T9_epBOOST/ASCII_MICHELA/ascii_daq_sshfs/*.dat | tail -1)"
+    run_number="${filepath##*/}"
+    run_number="${run_number#run}"
+    run_number="${run_number%%_*}"
 
-        echo $run_number
+    echo $run_number
 
-        # Update json
-        sed -i "s/\"run_number\": *[0-9]\+/\"run_number\": $run_number/" $currRunJsongi
+    # Update json
+    sed -i "s/\"run_number\": *[0-9]\+/\"run_number\": $run_number/" $currRunJson
 
-        # Execute jupyter
-        currFile="run${run_number}.html"
-        echo $currFile
-        jupyter nbconvert --execute --to html ${sourcefile}.ipynb
-        jupyter nbconvert --execute --to html ${sourcefile2}.ipynb
-        cp ${sourcefile}.html $targetdir/$currFile
-        cp ${sourcefile}.html $targetdir/current.html
-        cp ${sourcefile2}.html $targetdir/compare.html
+    # Execute jupyter
+    currFile="run${run_number}.html"
+    echo $currFile
+    jupyter nbconvert --execute --to html ${sourcefile}.ipynb
+    jupyter nbconvert --execute --to html ${sourcefile2}.ipynb
+    cp ${sourcefile}.html $targetdir/$currFile
+    cp ${sourcefile}.html $targetdir/current.html
+    cp ${sourcefile2}.html $targetdir/compare.html
 
-        sleep ${sleepsec}s
+    sleep ${sleepsec}s
 
 done
